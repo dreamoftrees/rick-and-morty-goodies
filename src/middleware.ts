482eba279eed34d92ebbc08890d2dde4371f5938
Charlie McKenzie <charlie@dreamoftrees.com>
2024-05-06 15:50:08 +1000

    Tweak to middleware for cookie values

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,7 +9,7 @@ export function middleware(request: NextRequest) {
   // TODO: Refactor this middleware into a composable function
   const nameToken = request.cookies.get('name');
   const titleToken = request.cookies.get('title');
-  if (nameToken && titleToken) {
+  if (nameToken?.value && nameToken?.value.length > 0 && titleToken?.value && titleToken?.value.length > 0) {
     return NextResponse.next();
   } else {
     // If the cookie does not exist, redirect to the login page
