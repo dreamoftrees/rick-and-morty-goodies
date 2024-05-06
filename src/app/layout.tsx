21f4237fab928eb64dbd8b690413c92ec6ed3ba2
Charlie McKenzie <charlie@dreamoftrees.com>
2024-05-06 20:56:24 +1000

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { Providers } from './providers';
 import styles from './layout.module.css';
 import { Box } from '@chakra-ui/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -37,6 +38,7 @@ export default function RootLayout({
           {children}
         </Providers>
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
